c96b29ccc2e0758343c6dc8634c252f3eed0c0c1
DESKTOP-MFP0DTB\Viviane <viviane.annen@students.fhnw.ch>
2016-11-22 17:19:42 +0100

    class structure .gitignore

---
+++ src/main/java/ch/fhnw/oop2/Buildings_main.java
@@ -0,0 +1,12 @@
+package ch.fhnw.oop2;
+
+/**
+ * Created by Viviane on 08.11.2016.
+ */
+
+public class Buildings_main {
+    public static void main(String[] args) {
+
+        System.out.println("Test");
+    }
+}
