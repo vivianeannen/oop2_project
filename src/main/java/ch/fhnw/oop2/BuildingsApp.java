3c6710551e2775ae091b3f9401fe9ecd463952ca
DESKTOP-MFP0DTB\Viviane <viviane.annen@students.fhnw.ch>
2016-11-26 12:31:28 +0100

    .fxml file new javafx class

---
+++ src/main/java/ch/fhnw/oop2/BuildingsApp.java
@@ -0,0 +1,17 @@
+package ch.fhnw.oop2;/**
+ * Created by Viviane on 26.11.2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BuildingsApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override public void start(Stage primaryStage) {
+
+    }
+}
