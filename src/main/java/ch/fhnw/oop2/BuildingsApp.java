13a09b2da70b77941951c2379c0351afaf1a98ea
DESKTOP-MFP0DTB\Viviane <viviane.annen@students.fhnw.ch>
2016-11-26 13:47:40 +0100

    BuildingsOverview RootLayout

--- src/main/java/ch/fhnw/oop2/BuildingsApp.java
+++ src/main/java/ch/fhnw/oop2/BuildingsApp.java
@@ -2,16 +2,79 @@ package ch.fhnw.oop2;/**
  * Created by Viviane on 26.11.2016.
  */
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class BuildingsApp extends Application {
+
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Buildings");
+
+        initRootLayout();
+
+        showBuildingsOverview();
+
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(BuildingsApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the person overview inside the root layout.
+     */
+    public void showBuildingsOverview() {
+        try {
+            // Load buildings overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(BuildingsApp.class.getResource("view/BuildingsOverview.fxml"));
+            AnchorPane buildingsOverview = (AnchorPane) loader.load();
+
+            // Set buildings overview into the center of root layout.
+            rootLayout.setCenter(buildingsOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     *
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override public void start(Stage primaryStage) {
-
-    }
 }
